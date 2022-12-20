9e42fc0beb95bd09f91a1debaf6ad20e7d8aa653
Hakyung Sohn <hortenssiaa@gmail.com>
2022-12-20 22:29:42 +0900

    commit itertools

--- daily-solving/day002-python-library.py
+++ daily-solving/day002-python-library.py
@@ -57,3 +57,52 @@ print(result) # [('김가나', 43), ('홍마바', 32), ('박다라', 20)]
 data = [9, 1, 8, 5, 4]
 data.sort()
 print(data) # [1, 4, 5, 8, 9]
+
+
+
+
+
+
+# 2. itertools
+'''
+- permutations (순열)
+: list와 같은 iterable객체에서 r개의 데이터를 뽑아 일렬로 나열하는 모든 경우
+
+- combinations (조합)
+: list와 같은 iterable객체에서 r개의 데이터를 뽑아 순서를 고려하지 않고 나열하는 모든 경우
+
+- product (중복 순열)
+: r개의 데이터를 뽑아 일렬로 나열하는 모든 경우 (원소 중복)
+
+- combination_with_replacement (중복 조합)
+: r개의 데이터를 뽑아 순서를 고려하지 않고 나열하는 모든 경우 (원소 중복)
+'''
+# permutations (순열) - 순서 O!
+from itertools import permutations
+data = ['A', 'B', 'C']
+result = list(permutations(data, 3)) # 모든 순열 구하기
+print(result) # [('A', 'B', 'C'), ('A', 'C', 'B'), ('B', 'A', 'C'), ('B', 'C', 'A'), ('C', 'A', 'B'), ('C', 'B', 'A')]
+
+# combinations (조합) - 순서 X
+from itertools import combinations
+result = list(combinations(data, 2)) # 2개를 뽑는 모든 조합 구하기
+print(result) # [('A', 'B'), ('A', 'C'), ('B', 'C')]
+
+result2 = list(combinations(data, 3))
+print(result2) # [('A', 'B', 'C')]
+
+# product (중복 순열) - 순서 O
+from itertools import product
+result = list(product(data, repeat=2)) # 2개를 뽑는 모든 순열 구하기 (중복 허용)
+print(result) # [('A', 'A'), ('A', 'B'), ('A', 'C'), ('B', 'A'), ('B', 'B'), ('B', 'C'), ('C', 'A'), ('C', 'B'), ('C', 'C')]
+
+result2 = list(product(data, repeat=3))
+print(result2) # [('A', 'A', 'A'), ('A', 'A', 'B'), ('A', 'A', 'C'), ('A', 'B', 'A'), ('A', 'B', 'B'), ('A', 'B', 'C'), ('A', 'C', 'A'), ('A', 'C', 'B'), ('A', 'C', 'C'), ('B', 'A', 'A'), ('B', 'A', 'B'), ('B', 'A', 'C'), ('B', 'B', 'A'), ('B', 'B', 'B'), ('B', 'B', 'C'), ('B', 'C', 'A'), ('B', 'C', 'B'), ('B', 'C', 'C'), ('C', 'A', 'A'), ('C', 'A', 'B'), ('C', 'A', 'C'), ('C', 'B', 'A'), ('C', 'B', 'B'), ('C', 'B', 'C'), ('C', 'C', 'A'), ('C', 'C', 'B'), ('C', 'C', 'C')]
+
+result3 = list(product(data, repeat=1))
+print(result3) # [('A',), ('B',), ('C',)]
+
+# combine_with_replacement (중복 조합) - 순서 X
+from itertools import combinations_with_replacement
+result = list(combinations_with_replacement(data, 2)) # 2개를 뽑는 모든 조합 구하기 (중복 허용)
+print(result) # [('A', 'A'), ('A', 'B'), ('A', 'C'), ('B', 'B'), ('B', 'C'), ('C', 'C')]
