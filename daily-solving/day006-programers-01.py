d0dbc4a4a56571327f6d04f1e35471d5afb1286d
Hakyung Sohn <hortenssiaa@gmail.com>
2023-01-12 18:44:10 +0900

    coding test preperation #w1-1 (update+1)

--- daily-solving/day006-programers-01.py
+++ daily-solving/day006-programers-01.py
@@ -3,6 +3,7 @@ parti : ["leo", "kiki", "eden"]
 comp : ["leo", "kiki"]
 '''
 
+# 처음 풀이
 participant = ["eden", "leo", "kiki", "leo"]
 completion = ["leo", "kiki"]
 
@@ -15,4 +16,37 @@ def solution(participant, completion):
     answer = participant[0]
     return answer
 
-print(solution(participant, completion))
+# print(solution(participant, completion))
+
+
+
+
+# 문제 풀이 - 해시인거 알고 다시 풀어보기
+# 해시
+# 문자열 이름을, 번호라고 생각하기
+
+# participant = ["eden", "leo", "kiki", "leo"]
+# completion = ["leo", "kiki"]
+participant = ["m", "s", "m", "a"]
+completion = ["s", "a", "m"]
+
+data = dict()
+def solution2(participant, completion):
+    answer = ""
+    for person in participant:
+        data[person] = participant.count(person)
+
+    for person in participant:
+        if data[person] == 2:
+            answer = person
+            break
+
+    return answer
+
+print(solution2(participant, completion))
+
+
+
+
+# 문제 풀이 - 답지
+# 해시
