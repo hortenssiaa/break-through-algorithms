948a89795cc53220a1779c221b4162efae3b8867
Hakyung Sohn <hortenssiaa@gmail.com>
2024-12-11 17:38:42 +0900

    (241211 백준) BFS_문제풀이

---
+++ algorithm_241211⭐️/boj_11724⭐️.py
@@ -0,0 +1,38 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+N, M = list(map(int, input().split()))
+
+adj = [[] for _ in range(N)]
+
+for _ in range(M):
+  u, v = list(map(int, input().split()))
+
+  # adj 구하기
+  adj[u - 1].append(v - 1)
+  adj[v - 1].append(u - 1)
+
+visit = [False] * N
+count = 0
+
+for i in range(N): # i: 0 F->T, 1 T, 2 T, 3 F
+  if visit[i]:
+    continue
+  
+  # BFS 시작
+  count += 1 # i:0 >count:1 // i:3 >count:2
+
+  queue = deque([i]) # 0 // 3
+  visit[i] = True # 0 F->T // 3 F->T
+  
+  while len(queue) != 0:
+    q = queue.popleft() # 0 T (x) // 1 T (o) >len(queue):1 // 2 T (O) >len(queue):0
+
+    for v in adj[q]: # 1 F, 2 F // 0 T, 2 T // 0 T, 1 T
+      if not visit[v]: 
+        visit[v] = True # 1 T, 2 T // 
+        queue.append(v) # 1 T (o), 2 T (o) >len(queue):2
+
+print(count)
